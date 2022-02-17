2d1997bcfdf3697e1bf44671d064043e084ec104
Rodrigo de Araujo Leite Pereira <rodrigoaraujo1709@gmail.com>
2022-02-17 08:40:02 -0300

    Seção4 - Aula 39 a 44

---
+++ EXERCICIOS-JS/Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,13 @@
+console.log(7 / 0)
+
+console.log ("10" / 2)
+
+console.log ("10" + 2) //concatenação de string, pois string tem preferência
+
+console.log ("Show!" * 2)
+
+console.log(0.1 + 0.7) //imprecisão
+
+//console.log(10.toString())
+
+console.log((10.354).toFixed(2))
