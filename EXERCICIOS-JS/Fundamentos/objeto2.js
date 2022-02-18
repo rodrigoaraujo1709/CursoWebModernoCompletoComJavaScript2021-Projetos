a058c3c0f8df2e077dc7ed8a06805869b7cb8dc3
Rodrigo de Araujo Leite Pereira <rodrigoaraujo1709@gmail.com>
2022-02-18 08:18:45 -0300

    Seção4 - Aula 51 a 60

---
+++ EXERCICIOS-JS/Fundamentos/objeto2.js
@@ -0,0 +1,10 @@
+console.log(typeof Number)
+console.log(typeof new Number())
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {} // ES 2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto())
