e0fe2f1e296006f9f28622ca6f80b47065090221
Rodrigo Pereira <96384298+cuprj-rodrigopereira@users.noreply.github.com>
2022-02-23 17:32:18 -0300

    Seção4 - Aula 60 a 75

---
+++ EXERCICIOS-JS/Fundamentos/erro.js
@@ -0,0 +1,26 @@
+function tratarErroElancar(erro){
+    //throw new Error('...')
+    //throw 10
+    //throw true
+    //throw 'mensagem'
+    throw{
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritando(obj){
+    try{
+        console.log(obj.name.toUpperCase() + '!!!')
+    }
+    catch(e){
+        tratarErroElancar(e)
+    }
+    finally{
+        console.log('final')
+    }
+}
+
+const obj = { nome: 'Roberto' } //correto seria name
+imprimirNomeGritando(obj)
